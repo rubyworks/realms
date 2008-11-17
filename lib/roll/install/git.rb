f06c9b769dc731f870f92aea66ae381a94dccf79
T. Sawyer <trans@di1525.(none)>
2008-11-17 10:32:41 -0500

    git.rb, use max version as default

--- lib/roll/install/git.rb
+++ lib/roll/install/git.rb
@@ -1,4 +1,4 @@
-require 'roll/install/base'
+require 'roll/install/scm'
 
 module Roll
 
@@ -10,97 +10,86 @@ module Roll
     # and revisions --they are just different labels on the
     # same thing.
     #
-    class Git < Base
+    # TODO: No doubt this could be implemented better, eg. with more
+    # knowledge of Git's capabilities. The goal for now though is
+    # just to get a working system.
+
+    class Git < Scm
 
       # Install the project.
       def install
-        if version
-          unless versions.include?(version)
-            raise "#{version} does not exist"
-          end
-        end
+        check_version
 
-        puts "Installing #{name}..."
-
-        clone
-
-        if version
-          dest = File.join(store, name, version)
-          system "git clone -l #{local_origin} #{dest}"
-
+        if File.exist?(destination)
+          update
+        else
+          puts "Installing #{name}..."
+          clone
           if $PRETEND
+            system "git clone -l #{origin} #{destination}"
             puts "cd #{dest}"
             system "git checkout #{version}"
             #git checkout --track -b {branch} origin/{branch}
           else
-            Dir.chdir(dest) do
+            Dir.chdir(destination) do
               system "git checkout #{version}"
               #git checkout --track -b {branch} origin/{branch}
             end
           end
-        else
-          dest = local_origin
         end
 
-        return dest
+        return destination
       end
 
-      #
+      # TODO: Maybe this should be "purge", and uninstall just removes it from the ledger.
       def uninstall
-        if version
-          dest = File.join(store, name, version)
-          unless File.directory?(dest)
-            raise "#{dest} does not exist"
-          end
-          FileUtils.rm_r(dest)
-        else
-
+        unless File.directory?(destination)
+          raise "#{version} is not installed"
         end
+        FileUtils.rm_r(destination)
       end
 
       # Update
       def update
-        if version
-          dest = File.join(store, name, version)
-          Dir.chdir(dest) do
-            system "git pull"
-          end
-        else
-          Dir.chdir(local_origin) do
-            system "git pull"
-          end
+        Dir.chdir(destination) do
+          system "git rebase #{version}"
         end
       end
 
       # Returns a list of existing versions/branches.
       def versions
-        vs = []
-        `git ls-remote #{uri}`.split("\n").each do |line|
-           rev, name = *line.split(/\s+/)
-           vs << name
-        end
-        vs.delete('HEAD')
-        vs.delete('refs/heads/master')
-        return vs
+        @versions ||= (
+          vs = []
+          `git ls-remote #{uri}`.split("\n").each do |line|
+             rev, name = *line.split(/\s+/)
+             vs << name
+          end
+          vs.delete('HEAD')
+          vs.delete('refs/heads/master')
+          return vs
+        )
       end
 
-      # Show versions.
-      #def show
-      #  vers = versions
-      #  if vers.empty?
-      #  else
-      #    puts versions.join("\n")
-      #  end
-      #end
+      def destination
+        File.join(store, name, version)
+      end
 
     private
 
-      #def local_origin
-      #  @local_origin ||= File.join(store, name, '0')
-      #end
+      # TODO: use natcmp for max
+      def version
+        @version ||= versions.max
+      end
 
       def clone
         system "git clone #{uri} #{origin}"
+      end
+
+      #
+      def check_version
+        if @version && !versions.include?(@version)
+          raise "#{@version} does not exist"
+        end
       end
 
     end#class Git
