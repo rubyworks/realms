2619c156ff0d259b66f35c38300eaf57bd330d69
trans <transfire@gmail.com>
2008-12-03 16:48:57 -0500

    cleanup of depot

--- depot/deprecated2/lib/roll/library/constants.rb
+++ depot/deprecated2/lib/roll/library/constants.rb
@@ -1,14 +1,4 @@
 class Library
-
-  # VersionError is raised when a requested version cannot be found.
-  class VersionError < ::RangeError  # :nodoc:
-  end
-
-  # VersionConflict is raised when selecting another version
-  # of a library when a previous version has already been selected.
-  class VersionConflict < ::LoadError  # :nodoc:
-  end
-
   # Roll configuration.
   #sysdir  = Config::CONFIG['sysconfdir']
   #sysfile = File.join(sysdir, 'roll.yaml')
--- depot/deprecated2/lib/roll/library/instance.rb
+++ depot/deprecated2/lib/roll/library/instance.rb
@@ -130,73 +130,13 @@ class Library
     #  @index ||= YAML::load(File.open(index_file)) #if index_file
     #end
 
-  # Path to library.
-  attr_reader :location
 
-  # Name of library.
-  attr_reader :name
 
-  # Version of library. This is a VersionNumber object.
-  attr_reader :version
-
-  # Release date.
-  attr_reader :date
-
-  # Status of project.
-  attr_reader :status
-
-  # Paths to look for files. This is not the same as the traditional
-  # $LOAD_PATH entry, which is often a directory above the libpath.
-  # For example, 'lib/' may be the loadpath, but 'lib/foo/'
-  # is the libpath. This is a significant difference between
-  # Roll and the traditional require system.
-  #
-  # In general this should include all the internal load paths,
-  # so long as there will be no name conflicts between directories.
-  attr_reader :libpath
-
-  # Library dependencies. These are libraries that will be searched
-  # if a file is not found in the main libpath.
-  attr_reader :depend
-
-  # Default library file. This is the default file to load if the library
-  # is required or loaded solely by it's own name. Eg. +require 'foo'+.
-  #
-  # If not specified it defaults to +require 'foo.rb'+.
-  attr_reader :default
-
-  # Alias for date.
-  alias_method :released, :date
-
-  # Inspection.
-  def inspect
-    if version
-      "#<Library #{name}/#{version}>"
-    else
-      "#<Library #{name}>"
-    end
-  end
-
-  # Compare by version.
-  def <=>(other)
-    version <=> other.version
-  end
 
   # Traditional loadpath(s). This is usually just 'lib'.
   # NOT USED B?C MAY NOT BE SET IF ONLY LIBPATH IS USED.
   #def loadpath ; @loadpath ; end
 
-  ###############
-  # DIRECTORIES #
-  ###############
-
-  # List of subdirectories that are searched when loading.
-  # This defualts to ['lib/{name}', 'lib']. The first entry is
-  # usually proper location; the latter is added for default 
-  # compatability with the traditional require system.
-  def libdir
-    libpath.collect { |path| File.join(location, path) }
-  end
 
 =begin
     #return if name == 'ruby' # NEED TO DO THIS BETTER.
@@ -218,102 +158,6 @@ class Library
     )
   end
 =end
-
-  alias_method :libdirs, :libdir
-
-  # Combines the libdirs and the libdirs of dependent libraries.
-  #
-  # TODO: Cache this.
-  def lib_search_path
-    libdirs + depend.collect{ |dl| dl.libdirs }.flatten #.uniq
-  end
-
-  # Location of binaries. This is alwasy bin/ or nil if a bin directory
-  # does not exist. This is a fixed convention, unlike lib/ which needs
-  # to be more flexable.
-  def bindir
-    @bindir ||= (
-      dir = File.join(location, 'bin')
-      File.directory?(dir) ? dir : nil
-    )
-  end
-
-  # Returns the path to the data directory, ie. {location}/data.
-  # Note that this does not look in the system's data share (usr/share/).
-  def datadir #(versionless=false)
-    File.join(location, 'data')
-    #     if version and not versionless
-    #       File.join(Config::CONFIG['datadir'], name, version)
-    #     else
-    #       File.join(Config::CONFIG['datadir'], name)
-    #     end
-  end
-
-  # Returns the path to the configuration directory, ie. {location}/etc.
-  # Note that this does not look in the system's configuration directory (etc/).
-  #
-  # TODO: This in particluar probably should look in the
-  # systems config directory-- maybe an overlay effect?
-  def confdir
-    File.join(location, 'etc')
-    #     if version
-    #       File.join(Config::CONFIG['confdir'], name, version)
-    #     else
-    #       File.join(Config::CONFIG['datadir'], name)
-    #     end
-  end
-
-  ##################
-  # REQUIRE & LOAD #
-  ##################
-
-  # Library specific #require.
-
-  def require(file)
-    if path = require_find(file)
-      Library.load_stack << self #name?
-      begin
-         success = Kernel.require(path)
-      ensure
-        Library.load_stack.pop
-      end
-      success
-    #elsif lib = depend.find{ |dl| dl.require_find(file) }
-    #  lib.require(file)
-    else
-      raise LoadError, "no such file to load -- #{name}:#{file}"
-    end
-  end
-
-  # Library specific load.
-
-  def load(file, wrap=nil)
-    if path = load_find(file)
-      Library.load_stack << self #name?
-      begin
-        success = Kernel.load(path, wrap)
-      ensure
-        Library.load_stack.pop
-      end
-      success
-    else
-      raise LoadError, "no such file to load -- #{name}:#{file}"
-    end
-  end
-
-  # Require find.
-  def require_find(file)
-    file = default if (file.nil? or file.empty?)
-    find = File.join('{' + lib_search_path.join(',') + '}', file + "{#{Library.dlext},.rb,}")
-    Dir.glob(find).first
-  end
-
-  # Load find.
-  def load_find(file)
-    file = default if (file.nil? or file.empty?)
-    find = File.join('{' + libdirs.join(',') + '}', file)
-    Dir.glob(find).first
-  end
 
   # Put the lib's load paths into the local lookup of the current library or
   # if at the toplevel, in the standard lookup.
