84aae1db6816eeb7e1fbbbd965dab6fe24b27f42
7rans <transfire@gmail.com>
2010-02-10 17:43:15 -0500

    minor: actually use metadata class

--- lib/roll/metadata.rb
+++ lib/roll/metadata.rb
@@ -1,16 +1,75 @@
 module Roll
 
   #--
-  # TODO: Use POM if available?
+  # TODO: Use POM? If available?
   #--
   class Metadata
 
     #TODO: hide most methods
 
+    attr :location
+
     #
     def initialize(location)
       @location = location
       @cache = {}
+    end
+
+    # Get library name.
+    def name
+      @cache[:name] ||= read('name')
+    end
+
+    # Get library version. 
+    #--
+    # TODO: handle VERSION file
+    # TODO: handle YAML
+    #++
+    def version
+      @cache[:version] ||= Version.new(read(:version))
+    end
+
+    # Get library active state.
+    def active
+      return @cache[:active] if @cache.key?(:active)
+      @cache[:active] = (
+        case read(:active).to_s.downcase
+        when 'false', 'no'
+          false
+        else
+          true
+        end
+      )
+    end
+
+    # Get library release date. 
+    #--
+    # TODO: default date to what?
+    #++
+    def released
+      @cache[:released] ||= read(:released) || "1900-01-01"
+    end
+
+    # Get library loadpath.
+    def loadpath
+      @cache[:loadpath] ||= (
+        val = read(:loadpath).to_s.strip.split(/\s*\n/)  # TODO: handle YAML
+        val = ['lib'] if val.empty?
+        val
+      )
+    end
+
+    #
+    def requires
+      @cache[:requires] ||= (
+        if entry = read(:requires)
+          entry.strip.split("\n").map do |line|
+            line.strip.split(/\s+/)
+          end
+        else
+          []
+        end
+      )
     end
 
     #
@@ -24,9 +83,11 @@ module Roll
 
   private
 
+    #--
     # TODO: handle YAML
+    #++
     def read(name)
-      file = Dir[File.join(location, "{meta,.meta}", name)].first
+      file = Dir[File.join(location, "{meta,.meta}", name.to_s)].first
       if file
         File.read(file).strip
       else
