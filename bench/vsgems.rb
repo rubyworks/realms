6ff3097ed361e1f703ad740ab2d3f7ec438a40ac
7rans <transfire@gmail.com>
2010-06-11 14:27:49 -0400

    improve benchmarks with open3

--- bench/vsgems.rb
+++ bench/vsgems.rb
@@ -1,11 +1,27 @@
+require 'open3'
 
-time_roll_init = `export RUBYOPT=""; ruby -e't=Time.now; require "roll"; puts Time.now - t'`.strip
-time_roll_reqs = `export RUBYOPT=""; ruby -e'require "roll"; t=Time.now; require "facets"; puts Time.now - t'`.strip
+def time(cmd)
+  t = Time.now
+  Open3.popen3(cmd) do |stdin, stdout, stderr|  
+    puts stdout unless stdout.read.empty?
+    puts stderr unless stderr.read.empty?
+  end
+  Time.now - t
+end
 
-time_gems_init = `export RUBYOPT=""; ruby -e't=Time.now; require "rubygems"; puts Time.now - t'`.strip
-time_gems_reqs = `export RUBYOPT=""; ruby -e'require "rubygems"; t=Time.now; require "facets"; puts Time.now - t'`.strip
+time_gems_opt = time %[export RUBYOPT=""; ruby -rubygems -e'nil']
+time_roll_opt = time %[export RUBYOPT=""; ruby -roll -e'nil']
 
-puts "      %20s %20s" % [ 'Roll', 'Gems' ]
-puts "Init: %20s %20s" % [ time_roll_init, time_gems_init ]
-puts "Reqs: %20s %20s" % [ time_roll_reqs, time_gems_reqs ]
+time_gems_req = time %[export RUBYOPT=""; ruby -e'require "rubygems"']
+time_roll_req = time %[export RUBYOPT=""; ruby -e'require "roll"']
+
+time_gems_use  = time %[export RUBYOPT=""; ruby -e'require "rubygems"; require "facets"']
+time_roll_use = time %[export RUBYOPT=""; ruby -e'require "roll"; require "facets"']
+
+puts "              %-16s %-16s" % [ 'Roll', 'Gems' ]
+puts "RUBYOPT :     %-16s %-16s" % [ time_roll_opt, time_gems_opt ]
+puts "REQUIRE :     %-16s %-16s" % [ time_roll_req, time_gems_req ]
+puts "UTILIZE :     %-16s %-16s" % [ time_roll_use, time_gems_use ]
+
+
 
--- bench/vsgems_bm.rb
+++ bench/vsgems_bm.rb
@@ -1,17 +1,25 @@
+require 'open3'
 require 'benchmark'
 
-puts "Ruby Roll"
-
-Benchmark.bmbm do |x|
-  x.report("Reqiure Roll: ") { system "ruby -roll -e'nil'" }
-  x.report("Require Facets via Roll: ") { system %[export RUBYOPT=""; ruby -roll -e'require "facets"'] }
+def run(cmd)
+  Open3.popen3(cmd) do |stdin, stdout, stderr|  
+    puts stdout unless stdout.read.empty?
+    puts stderr unless stderr.read.empty?
+  end
 end
 
-puts; puts
 puts "RubyGems"
 
-Benchmark.bmbm do |x|
-  x.report("Require Gems: ") { system "ruby -rubygems -e'nil'" }
-  x.report("Require Facets via Gems: ") { system %[export RUBYOPT=""; ruby -rubygems -e'require "facets"'] }
+Benchmark.bm(25) do |x|
+  x.report("Require Gems:            "){ run %[export RUBYOPT=""; ruby -rubygems -e'nil'] }
+  x.report("Require Facets via Gems: "){ run %[export RUBYOPT=""; ruby -rubygems -e'require "facets"'] }
+end
+
+puts
+puts "Ruby Roll"
+
+Benchmark.bm(25) do |x|
+  x.report("Reqiure Roll:            "){ run %[export RUBYOPT=""; ruby -roll -e'nil'] }
+  x.report("Require Facets via Roll: "){ run %[export RUBYOPT=""; ruby -roll -e'require "facets"'] }
 end
 
