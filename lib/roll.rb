1e7a9d04af286b1899ba895528cf507cb0204883
7rans <transfire@gmail.com>
2010-07-14 10:19:12 -0400

    minor: adjust require statements in roll.rb

--- lib/roll.rb
+++ lib/roll.rb
@@ -1,10 +1,6 @@
-require 'roll/original'
-require 'roll/config'
-require 'roll/library'
-require 'roll/ruby'
-require 'roll/kernel'  # require last
-
 module Roll
   VERSION = "1.3.0"  # TODO: make verison reference dynamic
 end
+
+require 'roll/kernel'  # require last
 
