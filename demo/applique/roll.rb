713241fb46be5ec840b255952044403163702607
7rans <transfire@gmail.com>
2010-07-15 22:58:06 -0400

    test: filling out qed tests

--- demo/applique/roll.rb
+++ demo/applique/roll.rb
@@ -1,8 +1,10 @@
-require 'fileutils'
+abort "Remove -roll from RUBYOPT before running tests." if ENV['RUBYOPT'].index('-roll')
 
 ENV['RUBYENV'] = nil
+ENV['roll_environment'] = nil
+ENV['roll_environment_stack'] = nil
 
-raise "Remove -roll from RUBYOPT before running tests." if ENV['RUBYOPT'].index('-roll')
+require 'fileutils'
 
 # Make sure we use local version of files.
 $:.unshift('lib')
@@ -15,33 +17,39 @@ require 'roll/config'
 ::Config::CONFIG_HOME.replace(File.expand_path('tmp/config'))
 ::Config::CACHE_HOME.replace(File.expand_path('tmp/cache'))
 
+# reset ledger
+#$LEDGER = Roll::Ledger.new
+
 # okay now we can require roll
 require 'roll'
 require 'roll/command'
 
 # let's do some pre-start checks
 
-raise "Not using temporary config location" unless ::Config::CONFIG_HOME == File.expand_path('tmp/config')
-raise "Not using temporary cache location" unless ::Config::CACHE_HOME == File.expand_path('tmp/cache')
+abort "Not using temporary config location" unless ::Config::CONFIG_HOME == File.expand_path('tmp/config')
+abort "Not using temporary cache location" unless ::Config::CACHE_HOME == File.expand_path('tmp/cache')
 
 env = Roll::Environment.new
 
 unless Roll::Environment::HOME == File.expand_path('tmp/config/roll/environments')
-  raise "Starting with incorrect HOME_ENV_DIR -- #{Roll::Environment::HOME_ENV_DIR}"
+  abort "Starting with incorrect HOME -- #{Roll::Environment::HOME}"
 end
 
 unless Roll::Environment::DEFAULT_FILE == File.expand_path('tmp/config/roll/default')
-  raise "Starting with incorrect DEFAULT_FILE -- #{Roll::Environment::DEFAULT_FILE}"
+  abort "Starting with incorrect DEFAULT_FILE -- #{Roll::Environment::DEFAULT_FILE}"
 end
 
-raise "Starting with incorrect default" unless Roll::Environment::DEFAULT == 'production'
-raise "Starting with non-default environment (#{env.name})" unless env.name == 'production'
-raise "Starting with non-empty environment" unless env.size == 0
+abort "Starting with incorrect default" unless Roll::Environment::DEFAULT == 'production'
+abort "Starting with non-default environment (#{env.name})" unless env.name == 'production'
+abort "Starting with non-empty environment" unless env.size == 0
 
 # Override shell operator to internal roll command.
 # TODO: capture stdout and stderr.
 def `(cmd)    #` for highlighter
-  if cmd.start_with?('roll')
+  case cmd
+  when /^roll\ use\ (.*?)$/
+    $LEDGER = Roll::Ledger.new($1.strip)  # pretty crazy, but should be ok for testing
+  when /^roll/
     cmd  = cmd.sub('roll', '').strip
     argv = *Shellwords.shellwords(cmd)
     out, err = capture do
