d28567840d5a6e487e502ff75f701f08ab17b5a0
7rans <transfire@gmail.com>
2010-07-15 23:46:00 -0400

    add which command

---
+++ lib/roll/commands/which.rb
@@ -0,0 +1,27 @@
+module Roll
+
+  class CommandWhich < Command
+    
+    #
+    def setup
+      op.banner = "Usage: roll which <path>"
+      op.separator "Display absolute path of library path."
+      #op.on('--all', '-a', "Search all environments.") do
+      #  opts[:all] = true
+      #end
+    end
+
+    #
+    def call
+      path = args.first
+      file = $LEDGER.find(path)
+      if file
+        puts file.fullname
+      else
+        puts "Not found."
+      end
+    end
+
+  end
+
+end
