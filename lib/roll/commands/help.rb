2d0ba388245b967b08f80dcc1bfb3d20f3e88f9c
7rans <transfire@gmail.com>
2010-07-15 23:00:39 -0400

    update help command with missing commands

--- lib/roll/commands/help.rb
+++ lib/roll/commands/help.rb
@@ -5,19 +5,23 @@ module Roll
 
     #
     def setup
+      spacer = "    %-32s %s"
+
       op.banner = "USAGE:\n   roll <COMMAND> [--OPT1 --OPT2 ...]\n\n" +
                   "Use 'roll <COMMAND> --help' for command details."
       op.separator " "
       op.separator "COMMANDS:"
-      op.separator "    use NAME  " + (" " * 23) + "Change current environment."
-      op.separator "    list      " + (" " * 23) + "List available environments."
-      op.separator "    in  [DIR] " + (" " * 23) + "Roll directory into current environment."
-      op.separator "    out [DIR] " + (" " * 23) + "Remove directory from current environment."
-      op.separator "    show      " + (" " * 23) + "Show current environment."
-      op.separator "    index     " + (" " * 23) + "Show current environment index."
-      op.separator "    sync      " + (" " * 23) + "Synchronize environment indexes."
-      op.separator "    path      " + (" " * 23) + "Output bin PATH list."
-      op.separator "    verify    " + (" " * 23) + "Verify project dependencies in current environment."
+      op.separator spacer % ["list               ", "List available environments."]
+      op.separator spacer % ["use NAME           ", "Change current environment. (Setting RUBYENV is better!)"]
+      op.separator spacer % ["in  [DIR]          ", "Roll directory into current environment."]
+      op.separator spacer % ["out [DIR]          ", "Remove directory from current environment."]
+      op.separator spacer % ["show               ", "Show current environment."]
+      op.separator spacer % ["sync               ", "Synchronize environment indexes."]
+      op.separator spacer % ["copy NAME1 [NAME2] ", "Copy environment to new environment."]
+      op.separator spacer % ["verify             ", "Verify project dependencies are in current environment."]
+      op.separator spacer % ["isolate            ", "Create an isolation index for present project."]
+      op.separator spacer % ["gem                ", "Run gem command, then resync any related environments."]
+      op.separator spacer % ["path               ", "Output bin PATH list."]
       op.separator " "
       op.separator "OPTIONS:"
     end
