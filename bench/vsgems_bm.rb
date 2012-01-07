008e3814968dd13a61b3c70895292dc5effac6fd
7rans <transfire@gmail.com>
2012-01-07 15:01:30 -0500

    Update benchmarks. [admin]

--- bench/vsgems_bm.rb
+++ bench/vsgems_bm.rb
@@ -18,21 +18,21 @@ def run(cmd)
 end
 
 puts
-puts "Ruby Roll"
-
-Benchmark.bm(25) do |x|
-  x.report("  Reqiure Roll:          "){ run %[ruby -roll -e'nil'] }
-  x.report("  Require Facets:        "){ run %[ruby -roll -e'require "facets"'] }
-  x.report("  Require Nokogiri:      "){ run %[ruby -roll -e'require "nokogiri"'] }
-end
-
-puts
 puts "RubyGems"
 
 Benchmark.bm(25) do |x|
   x.report("  Require Gems:          "){ run %[ruby -rubygems -e'nil'] }
   x.report("  Require Facets:        "){ run %[ruby -rubygems -e'require "facets"'] }
   x.report("  Require Nokogiri:      "){ run %[ruby -rubygems -e'require "nokogiri"'] }
+end
+
+puts
+puts "Ruby Roll"
+
+Benchmark.bm(25) do |x|
+  x.report("  Reqiure Roll:          "){ run %[ruby -roll -e'nil'] }
+  x.report("  Require Facets:        "){ run %[ruby -roll -e'require "facets"'] }
+  x.report("  Require Nokogiri:      "){ run %[ruby -roll -e'require "nokogiri"'] }
 end
 
 puts
