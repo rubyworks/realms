fa0929ae9a37b62c87ca6248171296a51ccc955d
7rans <transfire@gmail.com>
2013-01-16 08:08:06 -0500

    Remove old crufy code from isolate.rb.

--- lib/realms/shell/isolate.rb
+++ lib/realms/shell/isolate.rb
@@ -10,7 +10,7 @@ module Realms
         development = false
         format = nil
 
-        op.banner = "Usage: realm isolate"
+        op.banner = "Usage: realm isolate [PATH]"
         op.separator "Create an isolation template."
 
         op.on('--development', '-d', "include development dependencies.") do
@@ -21,13 +21,13 @@ module Realms
           format = :gem
         end
 
-        op.on('--yaml', '-y', "Generate a template in YAML format.") do
-          format = :yaml
-        end
+        #op.on('--yaml', '-y', "Generate a template in YAML format.") do
+        #  format = :yaml
+        #end
 
         parse
 
-        location = argv.first || Dir.pwd  # TODO: find root ?
+        location = argv.first || Dir.pwd  # TODO: Utils.locate_root ?
 
         library = $LOAD_MANAGER.add(location)
 
@@ -37,7 +37,7 @@ module Realms
     private
 
       #
-      #
+      # Generate isolation template.
       #
       def generate_isolation_template(library, development, format=nil)
         $LOAD_MANAGER.isolate_library(library, development)
@@ -52,7 +52,7 @@ module Realms
           end
         when :yaml
           puts $LOAD_MANAGER.map{ |name, lib|
-            { 'name'=>lib.name, 'version'=>lib.version.to_s } #, 'groups'=>lib.groups, 'development'=>lib.development? }
+            { 'name'=>lib.name, 'version'=>lib.version.to_s }
           }.to_yaml
         else
           $LOAD_MANAGER.each do |name, lib|
@@ -60,55 +60,6 @@ module Realms
           end
         end
       end
-
-
-=begin
-      # TODO: Move most of this code into library somewhere.
-      def generate_isolate_index(location, development=nil)
-        require 'fileutils'
-
-        #if opts[:all]
-        #  list = Roll.environments
-        #else
-        #  list = [Roll.environment.name]
-        #end
-
-        library = Library.new(location)
-        ledger  = $LOAD_MANAGER.dup
-
-        results = ledger.activate_requirements(library, development)
-
-        fails, libs = results.partition{ |r| Array === r }
-
-        if fails.empty?
-          out = ''
-
-          max_name = results.map{ |lib| lib.name.size }.max
-          max_path = results.map{ |lib| lib.location.size }.max
-
-          results.each do |lib|
-            out << "%-#{max_name}s  %-#{-max_path}s  %s\n" % [lib.name, lib.location, lib.loadpath.join(' ')]
-          end
-
-#          dir = Roll.config.local_environment_directory
-
-          #FileUtils.mkdir_p(dir)
-
-          #file = File.join(dir, 'local')
-
-          #File.open(file, 'w'){ |f| f << out }
-
-          $stdout.puts out
-          $stderr.puts
-          $stderr.puts "Saved to `#{file}`."
-        else
-          puts "These libraries could not be found in the current environment:"
-          fails.each do |name, vers|
-            $stderr.puts "  #{name} #{vers}"
-          end
-        end
-      end
-=end
 
     end
   end
